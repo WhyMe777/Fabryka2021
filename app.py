06e135dfb6795658df3aac71615ea6b67617cae8
WhyMe777 <52386187+WhyMe777@users.noreply.github.com>
2021-08-22 15:06:40 +0200

    kalkulator

---
+++ app.py
@@ -0,0 +1,5 @@
+x = input('Podaj imie: ')
+y = input('Podaj nazwisko: ')
+
+def add(x, y):
+    return x + y
--- homeworks/kalkulator.py
+++ homeworks/kalkulator.py
@@ -1,4 +1,5 @@
 # Cześć Paweł. Poniżej przygotowany kalkulator.
+from math import sqrt
 
 def add(x, y):
     return x + y
@@ -12,36 +13,50 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-print("""
+def remainder(x, y):
+    return x % y
+
+def power(x, y):
+    return x ** y
+
+print('''
 Wybierz operację:
  (1) dodawanie
  (2) odejmowanie
  (3) mnożenie
  (4) dzielenie
-""")
+ (5) reszta z dzielenia
+ (6) potęga
+''')
 
 while True:
-    choice = input("Wybierz operację (1|2|3|4): ")
+    choice = input('Wybierz operację (1|2|3|4|5|6): ')
 
-    if choice in ('1', '2', '3', '4'):
-        x = float(input("Podaj pierwszą liczbę: "))
-        y = float(input("Podaj drugą liczbę: "))
+    if choice in ('1', '2', '3', '4', '5', '6'):
+        x = float(input('Podaj pierwszą liczbę: '))
+        y = float(input('Podaj drugą liczbę: '))
 
 # Część dodana
 
-    if choice == "1":
+    if choice == '1':
         print(add(x,y))
-        break
+
     elif choice == '2':
         print(subtract(x, y))
-        break
+
     elif choice == '3':
         print(multiply(x, y))
-        break
-    elif choice == "4":
+
+    elif choice == '4':
         print(divide(x, y))
-        break
-    elif choice != ('1', '2', '3', '4'):
+
+    elif choice == '5':
+        print(remainder(x, y))
+
+    elif choice == '6':
+        print(power(x, y))
+
+    elif choice != ('1', '2', '3', '4', '5', '6'):
         print('''
 Wybrałeś niepoprawny numer działania.
 Wybierz ponownie.
