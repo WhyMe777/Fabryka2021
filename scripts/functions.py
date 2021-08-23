640dffd8413a50f59b818041d08ca9cb32ae9e8f
WhyMe777 <aborcz777@gmail.com>
2021-08-23 22:05:56 +0200

    functions

---
+++ scripts/functions.py
@@ -0,0 +1,8 @@
+def divide(divident, divisor):
+    if(divisor == 0):
+        return False
+    else:
+        return divident / divisor
+
+print(divide(5,0))
+print(divide(10,2))
---
+++ scripts/functions2.py
@@ -0,0 +1,4 @@
+def my_function(arg1, arg2 = 'Alfa Romeo'):
+    return f'{arg1} {arg2}'
+
+print(my_function(arg1='car', arg2='Ford'))
